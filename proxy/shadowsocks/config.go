441525ef504edfb4b9a91937eb218fdaec78b5e1
lizhengyong <lizhengyong@agora.io>
2022-08-21 11:57:56 +0800

    restore ss cipher

--- proxy/shadowsocks/config.go
+++ proxy/shadowsocks/config.go
@@ -67,6 +67,10 @@ func createXChaCha20Poly1305(key []byte) cipher.AEAD {
 
 func (a *Account) getCipher() (Cipher, error) {
 	switch a.CipherType {
+	case CipherType_AES_128_CFB:
+		return &AesCfb{KeyBytes: 16}, nil
+	case CipherType_AES_256_CFB:
+		return &AesCfb{KeyBytes: 32}, nil
 	case CipherType_AES_128_GCM:
 		return &AEADCipher{
 			KeyBytes:        16,
@@ -96,6 +100,53 @@ func (a *Account) getCipher() (Cipher, error) {
 	default:
 		return nil, newError("Unsupported cipher.")
 	}
+}
+
+// AesCfb represents all AES-CFB ciphers.
+type AesCfb struct {
+	KeyBytes int32
+}
+
+func (*AesCfb) IsAEAD() bool {
+	return false
+}
+
+func (v *AesCfb) KeySize() int32 {
+	return v.KeyBytes
+}
+
+func (v *AesCfb) IVSize() int32 {
+	return 16
+}
+
+func (v *AesCfb) NewEncryptionWriter(key []byte, iv []byte, writer io.Writer) (buf.Writer, error) {
+	stream := crypto.NewAesEncryptionStream(key, iv)
+	return &buf.SequentialWriter{Writer: crypto.NewCryptionWriter(stream, writer)}, nil
+}
+
+func (v *AesCfb) NewDecryptionReader(key []byte, iv []byte, reader io.Reader) (buf.Reader, error) {
+	stream := crypto.NewAesDecryptionStream(key, iv)
+	return &buf.SingleReader{
+		Reader: crypto.NewCryptionReader(stream, reader),
+	}, nil
+}
+
+func (v *AesCfb) EncodePacket(key []byte, b *buf.Buffer) error {
+	iv := b.BytesTo(v.IVSize())
+	stream := crypto.NewAesEncryptionStream(key, iv)
+	stream.XORKeyStream(b.BytesFrom(v.IVSize()), b.BytesFrom(v.IVSize()))
+	return nil
+}
+
+func (v *AesCfb) DecodePacket(key []byte, b *buf.Buffer) error {
+	if b.Len() <= v.IVSize() {
+		return newError("insufficient data: ", b.Len())
+	}
+	iv := b.BytesTo(v.IVSize())
+	stream := crypto.NewAesDecryptionStream(key, iv)
+	stream.XORKeyStream(b.BytesFrom(v.IVSize()), b.BytesFrom(v.IVSize()))
+	b.Advance(v.IVSize())
+	return nil
 }
 
 // AsAccount implements protocol.AsAccount.
